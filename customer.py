0f14b8f76e9051cbfb3621bb5473770dcdaef1c8
さいか <saikaraika555@gmail.com>
2020-01-10 21:38:05 +0900

    python

---
+++ customer.py
@@ -0,0 +1,11 @@
+#お客様リストのデータを作成します
+customer_list = [
+    '小泉晃一男46歳',
+    '羽賀敬男23歳',
+    '荒川美千子女18歳',
+    '下島理英子女36歳',
+    '斉藤稔男24歳']
+ 
+#お客様リストcustomer_listから一行ずつcustomerにいれます。
+for customer in customer_list:
+    print(customer) #customerを出力します。
